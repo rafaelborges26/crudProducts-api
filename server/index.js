4fe610c96da82e9ad0fd8281303ad84d5acde5d6
Rafael Borges <rafael.borges2698@gmail.com>
2020-06-11 01:50:09 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(require, response) => {
+ //objeto require da respostas do navegador, como ip, e outras coisas
+ //objeto response responde pro navegador
+        response.send("Helleo words!!")
+//instalamos o nodemon para stalvar as alterações de forma automatica
+})
+
+app.listen(3001)
+
