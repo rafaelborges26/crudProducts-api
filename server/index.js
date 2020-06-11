80def9ee23fe1161c0d9a06c75ec4cb6589436bd
Rafael Borges <rafael.borges2698@gmail.com>
2020-06-11 02:17:04 -0300

    testing

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/',(require, response) => {
  //objeto require da respostas do navegador, como ip, e outras coisas
  //objeto response responde pro navegador
-        response.send("Helleo words!!")
+        response.send("Hello words!!")
 //instalamos o nodemon para stalvar as alterações de forma automatica
 })
 
